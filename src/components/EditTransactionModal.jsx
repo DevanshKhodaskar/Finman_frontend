7d31497c625a22488455abbdc909a8ac50f728a3
DevanshKhodaskar <devanshkhodaskar870@gmail.com>
2025-12-21 10:52:34 +0530

    cancel button color changed

--- src/components/EditTransactionModal.jsx
+++ src/components/EditTransactionModal.jsx
@@ -225,13 +225,14 @@ const EditTransactionModal = ({ isOpen, onClose, transaction, onSuccess }) => {
             />
           </div>
 
-          {/* Buttons */}
-          <div className="flex gap-2 pt-3">
-            <button
-              type="button"
-              onClick={onClose}
-              className="flex-1 py-2 bg-slate-100 rounded-lg"
-            >
+        {/* Buttons */}
+<div className="flex gap-2 pt-3">
+  <button
+    type="button"
+    onClick={onClose}
+    className="flex-1 py-2 bg-slate-100 text-black rounded-lg"
+  >
+
               Cancel
             </button>
             <button
